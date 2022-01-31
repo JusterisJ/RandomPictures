6fed8f786998f58b27bbbce91c98cb5060a2ad5d
JusterisJ <justasjusteris@gmail.com>
2022-01-31 09:21:44 +0200

    hh

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let buttonNewPicture = document.querySelector(`.newPicture`);
 let pictureDiv = document.querySelector(`.picture`);
 let randomText = document.querySelector(`.randomText`);
+let previousRandomNum = Number();
 let data;
 fetch(
   `https://raw.githubusercontent.com/JusterisJ/RandomPictures/main/data.json`
@@ -13,20 +14,12 @@ function imageLoad(duomenys) {
   console.log(data);
 }
 
-let previousRandomNum = Number();
 buttonNewPicture.addEventListener(`click`, function () {
   changePictureAndQuote();
-});
-window.addEventListener(`keyup`, function (event) {
-  if (event.keyCode == 70) {
-    console.log(`1text`);
-  }
 });
 
 function changePictureAndQuote() {
   let randomNum = random();
-
-  console.log(randomNum);
   if (randomNum == previousRandomNum) {
     if ((previousRandomNum = !0)) {
       randomNum--;
@@ -38,10 +31,6 @@ function changePictureAndQuote() {
   pictureDiv.innerHTML = `<img src=${data[randomNum].src} alt="" />`;
   randomText.textContent = data[randomNum].quote;
 }
-
 function random() {
   return Math.floor(Math.random() * (data.length - 1 - 0 + 1)) + 0;
-}
-for (let i = 0; i < 100; i++) {
-  console.log(random());
 }
